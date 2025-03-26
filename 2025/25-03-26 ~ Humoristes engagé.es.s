f62a6490ee6ffd253f7ecd2488eaf0d3c1dadaf6
TremaîDayo <tremaidayo@gmail.com>
2025-03-26 17:59:49 +0100

    Create 25-03-26 ~ Humoristes engagé.es.s

---
+++ 2025/25-03-26 ~ Humoristes engagé.es.s
@@ -0,0 +1,11 @@
+C’est non sans bravour que certains usent d’humour pour dédramatiser les sujets les plus lourds.
+Que ce soit par amour de leur art ou des autres sans détour d’habiles calembours ils profèrent dénonçant les tourments de nos malmenées sociétés.
+Tantôt provocateur, tantôt évocateur, c’est avec ardeur et non dénué de rigueur que notre humeur ils mettent en sueur avec leurs propos à la déconcertante teneur.
+Par leurs plumes, mots et silences il dépeignent un monde en décadence où trônent les outrances.
+Mais les rires qu’ils déclenchent sonnent la fin de cette triste danse et donne naissance à ce qui fait notre essence.
+La jouissance de notre immuable condition humaine pleine de ses joies et peines.
+Or sans ces joyeuse mises en perspectives empreinte d’empathie plus compliqué il serait de se relever de certains faits où notre triste habitude est de se détourner.
+Par leurs éclats stylistiques ils mettent à l’honneur une esthétique diabolique créée pour nous entrainer vers un monde plus buccolique où se confondent souffrance idyllique et plaisir satirique.
+Qu’elles soient rustres ou sophisitiqués, abordant des sujets lourds ou légers, vos comiques épopées de marbre ne m’ont jamais laissées.
+Ainsi en moi vos idéaux vous avez gravés et sans cesse je tâche de les relayer pour faire vivre cette humanité riche de toutes ses diversités sans lesquelles nous ne pourrions pleinement profiter.
+Si à cette écoute un sourrire vous avez esquissé, ne vous reste plus que de vous même sans gêne rigoler pour compléter ce moment de félicité.
